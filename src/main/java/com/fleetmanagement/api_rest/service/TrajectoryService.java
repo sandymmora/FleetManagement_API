9731c25ba68846f1a2238212f0c1f784cf657d9b
DESKTOP-A44G8EQ\Sandy <sandy.mmora@gmail.com>
2024-07-01 21:33:10 -0600

    Excepciones y test para endpoint

--- src/main/java/com/fleetmanagement/api_rest/service/TrajectoryService.java
+++ src/main/java/com/fleetmanagement/api_rest/service/TrajectoryService.java
@@ -2,6 +2,7 @@ package com.fleetmanagement.api_rest.service;
 
 import com.fleetmanagement.api_rest.dto.LatestTrajectoriesDTO;
 import com.fleetmanagement.api_rest.dto.TrajectoryDTO;
+import com.fleetmanagement.api_rest.exception.NotFoundException;
 import com.fleetmanagement.api_rest.mapper.LatestTrajectoriesDTOMapper;
 import com.fleetmanagement.api_rest.mapper.TrajectoryDTOMapper;
 import com.fleetmanagement.api_rest.repository.TrajectoryRepository;
@@ -25,6 +26,9 @@ public class TrajectoryService {
     }
 
     public List<TrajectoryDTO> getTrajectories(Integer taxi, String date){
+        if(trajectoryRepository.findByTaxi(taxi, date).isEmpty()){
+            throw new NotFoundException("Register not found");
+        }
         return trajectoryRepository.findByTaxi(taxi, date)
                 .stream()
                 .map(trajectoryDTOMapper)
