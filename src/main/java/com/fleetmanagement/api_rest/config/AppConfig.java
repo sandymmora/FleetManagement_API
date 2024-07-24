29dcaaa3fa33b252d28cf15328daff2e1ab01cff
DESKTOP-A44G8EQ\Sandy <sandy.mmora@gmail.com>
2024-07-23 20:10:45 -0600

    Proteccion de enpoints y crear arvicho de excel

--- src/main/java/com/fleetmanagement/api_rest/config/AppConfig.java
+++ src/main/java/com/fleetmanagement/api_rest/config/AppConfig.java
@@ -5,6 +5,8 @@ import com.fleetmanagement.api_rest.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -14,6 +16,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Properties;
 
 @Configuration
 @RequiredArgsConstructor
@@ -44,4 +48,22 @@ public class AppConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+//    @Bean
+//    public JavaMailSender getJavaMailSender() {
+//        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//        mailSender.setHost("smtp.gmail.com");
+//        mailSender.setPort(587);
+//
+//        mailSender.setUsername("my.gmail@gmail.com");
+//        mailSender.setPassword("password");
+//
+//        Properties props = mailSender.getJavaMailProperties();
+//        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.smtp.auth", "true");
+//        props.put("mail.smtp.starttls.enable", "true");
+//        props.put("mail.debug", "true");
+//
+//        return mailSender;
+//    }
 }
--- src/main/java/com/fleetmanagement/api_rest/controller/TrajectoryController.java
+++ src/main/java/com/fleetmanagement/api_rest/controller/TrajectoryController.java
@@ -2,14 +2,25 @@ package com.fleetmanagement.api_rest.controller;
 
 import com.fleetmanagement.api_rest.dto.LatestTrajectoriesDTO;
 import com.fleetmanagement.api_rest.dto.TrajectoryDTO;
-import com.fleetmanagement.api_rest.model.TrajectoryModel;
+
 import com.fleetmanagement.api_rest.service.TrajectoryService;
+import lombok.Data;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Date;
 import java.util.List;
 
 @RestController
@@ -20,13 +31,46 @@ public class TrajectoryController {
     TrajectoryService trajectoryService;
 
     @GetMapping()
-    public List<TrajectoryDTO> getTrajectories(@RequestParam String taxiId,
+    public List<TrajectoryDTO> getTrajectories(@RequestParam Integer  taxiId,
                                                @RequestParam String date){
-        Integer taxi = Integer.parseInt(taxiId);
-        return trajectoryService.getTrajectories(taxi,date);
+        return trajectoryService.getTrajectories(taxiId,date);
     }
     @GetMapping("/latest")
     public List<LatestTrajectoriesDTO> getLatestTrajectories(){
         return trajectoryService.getLatestTrajectories();
+    }
+    @GetMapping("/export")
+    public String trajectoriesExcel(@RequestParam Integer taxiId,
+                                    @RequestParam String date) throws IOException {
+
+        List<TrajectoryDTO> trajectories = trajectoryService.getTrajectories(taxiId, date);
+
+        Workbook workbook = new XSSFWorkbook();
+
+        Sheet sheet = workbook.createSheet("Trajectories");
+
+        for(int r=0; r<trajectories.size(); r++){
+            Row row = sheet.createRow(r);
+                Cell cellId = row.createCell(0);
+                cellId.setCellValue(String.valueOf(trajectories.get(r).getId()));
+                Cell cellPlate = row.createCell(1);
+                cellPlate.setCellValue(String.valueOf(trajectories.get(r).getPlate( )));
+                Cell cellDate = row.createCell(2);
+                cellDate.setCellValue(String.valueOf(trajectories.get(r).getDate().toLocalDateTime()));
+                Cell cellLat = row.createCell(3);
+                cellLat.setCellValue(String.valueOf(trajectories.get(r).getLatitude()));
+                Cell cellLong = row.createCell(4);
+                cellLong.setCellValue(String.valueOf(trajectories.get(r).getLongitude()));
+
+        }
+
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        String fileLocation = path.substring(0, path.length() - 1) + "temp.xlsx";
+
+        FileOutputStream outputStream = new FileOutputStream(fileLocation);
+        workbook.write(outputStream);
+        workbook.close();
+        return "Excel creado";
     }
 }
